6b3b7b47b7e92488197d261df0e2158672137fa2
SantiagoQuispe <103156800+SantiagoQuispe@users.noreply.github.com>
2022-05-11 21:54:58 -0300

    Modificaciones del TP
    Se agregaron nuevas formas de comparaciones entre caracteres.

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -10,8 +10,8 @@ using namespace std::literals;
 
 int main()
 {
-    //Asserts para datos de tipo booleano
-    assert(true);
+	//Asserts para datos de tipo booleano
+	assert(true);
     assert(false==false);
     assert(true!=false);
     assert(not false);
@@ -21,24 +21,27 @@ int main()
     assert(true and true);
     assert(false or true and false==false);
     //Asserts para datos de tipo double (números decimales cuya precisión es variable.)
-    assert(3.001==2.0+1.001);
-    assert(10.5==21.0/2.0);
-    assert(4.0==2.8+1.2);
-    assert(2==1+1.0);
+    assert(3.14156 > 2.789874);
+    assert(1.72785 > 1.173789 and 2.5/0.01 > 10);
+    assert(3.5*20 > 90/4 or 1/6 < 1/2);
     //Asserts para datos de tipo string (cadena de caracteres)
     assert("Hola, mundo!"s=="Hola,"s+" mundo!"s);
     assert("String (cadena)"s.length()==15); //Longitud del string (texto)
     assert("Día 1"s.length() == 6); //Caracter con tilde cuenta como 2 caracteres.
+    assert('hh' > 'hg');//Evalúa caracter a caracter.
     //Asserts para datos de tipo int (números enteros desde -2,147,483,648 hasta 2,147,483,647)
     assert(-4 == -4);
-    assert(17+4 == 21);
-    assert(-78 == 156/(-2));
+    assert(17+4 != 20);
+    assert(-78 > 160/(-2));
+    assert(-5 < 3 and 5 > 2);
     //Asserts para datos de tipo char (caracter almacenado en 1 byte)
-    assert("abc" == "abc");
-    assert("4568" == "4568");
-    assert("re78" == "re78");
+    assert("h" == "h");
+    assert('h' == 104);
+    assert("!" < "?");
+    assert("h" > "g" or "j" < "k");
     //Asserts para datos de tipo unsigned (números naturales desde 0 hasta 65535)
     assert(4568 == 4568);
     assert(787 >782);
-    assert(2574 == (2000 + 500 + 70 + 4));
+    assert(34 > 43 or 85 < 100);
+    assert(24 < 134 and 43 > 20);
 }
